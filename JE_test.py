c7279ce74a16f9c94481f8db1b18553a8fc6b29d
JE <nl21501@bristol.ac.uk>
2022-03-15 12:16:22 +0000

    neuriteness and edges

--- JE_test.py
+++ JE_test.py
@@ -4,7 +4,15 @@
 from skimage import io, filters
 import os
 
-image = io.imread('data/uob_image_set/11059585/11059585_0.jpg')
+set_ID = '11059585'
+photo_ID = '0'
+
+conc_ID = set_ID + '/' + set_ID + '_' + photo_ID + '.jpg'
+
+path = 'data/uob_image_set/'
+
+filename = os.path.join(path,conc_ID)
+image = io.imread(filename)
 
 # ... or any other NumPy array!
 mejeiring = filters.meijering(image)
