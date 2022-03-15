82099e7e0c615c7fbfdeca29ebedb92d06175c89
JE <nl21501@bristol.ac.uk>
2022-03-15 15:26:42 +0000

    get features added

---
+++ exploration/features.py
@@ -0,0 +1,14 @@
+# %%
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage.transform import resize
+from skimage import io
+from skimage.color import rgb2gray
+from tqdm import tqdm
+
+def get_features(filepath):
+
+    
+
+    return mean,mode
