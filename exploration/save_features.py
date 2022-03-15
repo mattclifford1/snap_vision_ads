72b16e0b91f2ab50429dfd6a4f9c6cf37906b881
mattclifford1 <mc15445@my.bristol.ac.uk>
2022-03-15 15:51:56 +0000

    compute and save new feautres

--- exploration/save_features.py
+++ exploration/save_features.py
@@ -1,10 +1,26 @@
 import pandas as pd
+import numpy as np
 from argparse import ArgumentParser
-import features
+from exploration.features import get_features
 
 def get_all_features(csv_file):
-    database = pd.read_csv(csv_file)
-    print(database.head())
+    df = pd.read_csv(csv_file)
+    new_features = []
+    for index, row in df.iterrows():
+        image_path = row['image_path']
+        # calculate new feauters
+        features_dict = get_features(image_path)
+        # save features
+        new_features.append(list(features_dict.values()))
+    # add new features to df
+    new_features = np.array(new_features)
+    col = 0
+    for key in features_dict.keys():
+        df['key'] = new_features[:, col]
+        col += 1
+    print(df.head())
+
+
 
 
 
