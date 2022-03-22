6db5dfc6fd3dce860d1a28798496b0ac8f41a762
mattclifford1 <mc15445@my.bristol.ac.uk>
2022-03-22 13:43:25 +0000

    label added to database

--- wrangling/database_creator.py
+++ wrangling/database_creator.py
@@ -21,11 +21,16 @@ def contruct_database(dir):
     all_subdirs = get_all_subdirs(dir)
     all_images = []
     similar = []
+    labels = []
     for dir in all_subdirs:
         # get all image paths
         images = []
         for image_file in os.listdir(dir):
             images.append(os.path.join(dir, image_file))
+            head, label = os.path.split(dir)
+            if label == '':   # dir ended in '/'
+                _, label = os.path.split(head)
+            labels.append(label)
         # add images with their similar images from the dir
         for image_path in images:
             all_images.append(image_path)
@@ -33,7 +38,8 @@ def contruct_database(dir):
         # print(' ')
     # print(all_images)
     df = pd.DataFrame(data={'image_path': all_images,
-                            'similar_images': similar})
+                            'similar_images': similar,
+                            'label': labels})
     # print(df.head())
     folder = 'wrangling'
     save_file = 'image_paths.csv'
