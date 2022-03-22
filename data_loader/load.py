e6dfce242a6451d5095d69240995dc06c9e82e41
Matt Clifford <mc15445@my.bristol.ac.uk>
2022-03-22 18:15:51 +0000

    triplet loss loader

--- data_loader/load.py
+++ data_loader/load.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import os
+from skimage import io
+import random
+import ast
 
 
 class get_database:
@@ -32,12 +35,43 @@ class get_database:
     def __getitem__(self, i):
         row = self.df.iloc[i]
         return row
-        # return {'image_path': row['image_path'],
-        #         'similar_paths': row['similar_images']}
 
+
+
+'''
+to be used with torch's dataloader when training with triplet loss
+'''
+class get_training_data:
+    def __init__(self, df_csv='wrangling/image_paths.csv'):
+        self.df_csv = df_csv
+        self.df = pd.read_csv(self.df_csv)
+        self.image_paths = self.df['image_path'].tolist()
+        self.similar_images = self.df['similar_images'].tolist()
+        # convert str of similar images to lists
+        for i in range(len(self.similar_images)):
+            self.similar_images[i] = ast.literal_eval(self.similar_images[i])
+
+    def __len__(self):
+        return self.df.shape[0]
+
+    def __getitem__(self, i):
+        # get file paths for im, pos, neg example
+        image_path = self.image_paths[i]
+        random_similar = random.choice(self.similar_images[i])
+        not_similars = list(set(self.image_paths) - set(self.similar_images[i]))
+        random_not_similar = random.choice(not_similars)
+        # read images from disk
+        image_path = io.imread(image_path)
+        random_similar = io.imread(random_similar)
+        random_not_similar = io.imread(random_not_similar)
+        return {'image':    image_path,
+                'positive': random_similar,
+                'negative': random_not_similar}
 
 
 if __name__ == '__main__':
     data = get_database()
-    print(len(data))
-    print(data[0])
+    # print(len(data))
+    # print(data[0])
+    training_data = get_training_data()
+    print(training_data[0])
