f1113c6416d5dd6dd6fb8c389c80d9aa440ac837
Matt Clifford <mc15445@my.bristol.ac.uk>
2022-03-22 17:03:09 +0000

    parallisation

--- evaluation/compare_similar.py
+++ evaluation/compare_similar.py
@@ -1,32 +1,49 @@
 '''
 compare to nearest embeddings and see if they are of the same class
 '''
+import multiprocessing
 from sklearn.neighbors import KNeighborsClassifier
 from tqdm import tqdm
 
 
 class eval:
-    def __init__(self, embeddings, labels, num_neighbours=3):
+    def __init__(self, embeddings, labels, num_neighbours=3, compute_sequencially=False):
         self.embeddings = embeddings   # list
         self.labels = labels           # list
         self.num_neighbours = num_neighbours
+        self.compute_sequencially = compute_sequencially
         self.compute()
 
     def compute(self):
-        scores = []
         print('Running evaluation')
-        for i in tqdm(range(len(self.labels))):
-            X_train = list(self.embeddings) # copy
-            y_train = list(self.labels) # copy
-            x_test = X_train.pop(i)
-            y_test = y_train.pop(i)
-            y_pred = self.get_knn_class(X_train, y_train, [x_test])
-            if y_pred[0] == y_test:
-                scores.append(1)
-            else:
-                scores.append(0)
+        if self.compute_sequencially:
+            scores = self.run_sequentially()
+        else:
+            scores = self.run_parellel()
         self.accuracy = (sum(scores)/len(scores))
         print('Accuracy: ', self.accuracy*100, '%')
+
+    def run_sequentially(self):
+        scores = []
+        for i in tqdm(range(len(self.labels))):
+            scores.append(self.get_score(i))
+        return scores
+
+    def run_parellel(self):
+        pool_obj = multiprocessing.Pool()
+        scores = pool_obj.map(self.get_score,range(len(self.labels)))
+        return scores
+
+    def get_score(self, i):
+        X_train = list(self.embeddings) # copy
+        y_train = list(self.labels) # copy
+        x_test = X_train.pop(i)
+        y_test = y_train.pop(i)
+        y_pred = self.get_knn_class(X_train, y_train, [x_test])
+        if y_pred[0] == y_test:
+            return 1
+        else:
+            return 0
 
     def get_knn_class(self, X_train, y_train, x_test):
         neigh = KNeighborsClassifier(self.num_neighbours)
--- evaluation/readme.md
+++ evaluation/readme.md
@@ -8,3 +8,6 @@ labels = ['a', 'b', 'b', 'a', 'a']
 evaluation = compare_similar.eval(embeddings, labels)  # will print out accuracy
 accuracy = evaluation.accuracy    # accessing accuracy data
 ```
+
+Evalution is done by taking the nearest embeddings of a data point's embedding and assigning it the same class.
+Evaluation is sped up by using the `multiprocessing` python library to compute whether the closest embeddings are correct or not in parallel.
