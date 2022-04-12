7152cc3f059707681286942495629c70801b34fb
JE <nl21501@bristol.ac.uk>
2022-04-12 10:39:02 +0100

    jerskine colour filter madess

---
+++ exploration/colour_utilities.py
@@ -0,0 +1,106 @@
+
+
+import colorsys
+
+from PIL import Image
+from sklearn.cluster import KMeans
+import wcag_contrast_ratio as contrast
+
+
+def get_dominant_colours(path, *, count):
+    """
+    Return a list of the dominant RGB colours in the image at ``path``.
+
+    :param path: Path to the image file.
+    :param count: Number of dominant colours to find.
+
+    """
+    im = Image.open(path)
+
+    # Resizing means less pixels to handle, so the *k*-means clustering converges
+    # faster.  Small details are lost, but the main details will be preserved.
+    im = im.resize((100, 100))
+
+    # Ensure the image is RGB, and use RGB values in [0, 1] for consistency
+    # with operations elsewhere.
+    im = im.convert("RGB")
+    colors = [(r / 255, g / 255, b / 255) for (r, g, b) in im.getdata()]
+
+    return KMeans(n_clusters=count).fit(colors).cluster_centers_
+
+def coloured_square(hex_string):
+    """
+    Returns a coloured square that you can print to a terminal.
+    """
+    hex_string = hex_string.strip("#")
+    assert len(hex_string) == 6
+    red = int(hex_string[:2], 16)
+    green = int(hex_string[2:4], 16)
+    blue = int(hex_string[4:6], 16)
+
+    return f"\033[48:2::{red}:{green}:{blue}m \033[49m"
+
+
+# example tint_color = choose_tint_color(dominant_colors, background_color=(0, 0, 0))
+def choose_tint_color(dominant_colors, background_color):
+    # The minimum contrast ratio for text and background to meet WCAG AA
+    # is 4.5:1, so discard any dominant colours with a lower contrast.
+    sufficient_contrast_colors = [
+        col
+        for col in dominant_colors
+        if contrast.rgb(col, background_color) >= 4.5
+    ]
+
+    # If none of the dominant colours meet WCAG AA with the background,
+    # try again with black and white -- every colour in the RGB space
+    # has a contrast ratio of 4.5:1 with at least one of these, so we'll
+    # get a tint colour, even if it's not a good one.
+    #
+    # Note: you could modify the dominant colours until one of them
+    # has sufficient contrast, but that's omitted here because it adds
+    # a lot of complexity for a relatively unusual case.
+    if not sufficient_contrast_colors:
+        return choose_tint_color(
+            dominant_colors=dominant_colors + [(0, 0, 0), (1, 1, 1)],
+            background_color=background_color
+        )
+
+    # Of the colours with sufficient contrast, pick the one with the
+    # closest brightness (in the HSV colour space) to the background
+    # colour.  This means we don't get very dark or very light colours,
+    # but more bright, vibrant colours.
+    hsv_background = colorsys.rgb_to_hsv(*background_color)
+    hsv_candidates = {
+        tuple(rgb_col): colorsys.rgb_to_hsv(*rgb_col)
+        for rgb_col in sufficient_contrast_colors
+    }
+
+    candidates_by_brightness_diff = {
+        rgb_col: abs(hsv_col[2] - hsv_background[2])
+        for rgb_col, hsv_col in hsv_candidates.items()
+    }
+
+    rgb_choice, _ = min(
+        candidates_by_brightness_diff.items(),
+        key=lambda t: t[1]
+    )
+
+    assert rgb_choice in dominant_colors
+    return rgb_choice
+
+
+
+# if __name__ == "__main__":
+#     import sys
+
+#     try:
+#         path = sys.argv[1]
+#     except ImportError:
+#         sys.exit(f"Usage: {__file__} <PATH>")
+
+#     dominant_colors = get_dominant_colours(path, count=5)
+#     tint_color = choose_tint_color(dominant_colors, background_color=(0, 0, 0))
+
+#     print(tint_color)
+#     print("#%02x%02x%02x" % tuple(int(v * 255) for v in tint_color))
+# # %%
--- testing/JE_test.py
+++ testing/JE_test.py
@@ -1,26 +1,193 @@
 # %%
+# %load_ext autoreload
 
+# %autoreload 2
 
-from skimage import io, filters
+# %%
+from __future__ import print_function
+from skimage import io, color,filters
+import numpy as np
 import os
+from tqdm import tqdm
+
+import binascii
+import struct
+from PIL import Image
+import numpy as np
+import scipy
+import scipy.misc
+import scipy.cluster
+from sklearn.cluster import KMeans
+from os import walk
+
+# %%
+from colour_utilities import get_dominant_colours, choose_tint_color, coloured_square
+
+
 
 set_ID = '11059585'
 photo_ID = '0'
 
 conc_ID = set_ID + '/' + set_ID + '_' + photo_ID + '.jpg'
 
-path = 'data/uob_image_set/'
+path = '../data/uob_image_set/'
 
 filename = os.path.join(path,conc_ID)
+# filename = '/Users/jonathanerskine/Courses/Applied_Data_Science/CWK/snap_vision_ads/snap_vision_ads/data/uob_image_set/11059585/11059585_0.jpg'
+filename = '/Users/jonathanerskine/Courses/Applied_Data_Science/CWK/snap_vision_ads/snap_vision_ads/data/uob_image_set/11059585/11059585_0.jpg'
 image = io.imread(filename)
 
-# ... or any other NumPy array!
-mejeiring = filters.meijering(image)
-edges = filters.sobel(image)
+# %%
+n = 5
 
-io.imshow(mejeiring)
-io.show()
+# print(n,' : \n',image[n])
+# imshape = np.shape(image)
 
-io.imshow(edges)
-io.show()
+# unique_colours = {'colour':[],'count':[]}
 
+# for i in tqdm(range(0,imshape[0])):
+#     for j in range(0,imshape[1]):
+#         col_array = list(image[i,j])
+#         if col_array in unique_colours['colour']:
+#             # print('match')
+#             unique_colours['count'][unique_colours['colour'].index(col_array)]+=1
+#         else:
+#             # print('new colour')
+#             unique_colours['colour'].append(col_array)
+#             unique_colours['count'].append(1)
+
+# %%
+#  create positive and negative examples
+# path | id | number | dom_colours | 
+set1 = '/Users/jonathanerskine/Courses/Applied_Data_Science/CWK/snap_vision_ads/snap_vision_ads/data/uob_image_set/11059585'
+set2 = '/Users/jonathanerskine/Courses/Applied_Data_Science/CWK/snap_vision_ads/snap_vision_ads/data/uob_image_set/11233998'
+set3 = '/Users/jonathanerskine/Courses/Applied_Data_Science/CWK/snap_vision_ads/snap_vision_ads/data/uob_image_set/16013076'
+set4 = '/Users/jonathanerskine/Courses/Applied_Data_Science/CWK/snap_vision_ads/snap_vision_ads/data/uob_image_set/16196265'
+set5 = '/Users/jonathanerskine/Courses/Applied_Data_Science/CWK/snap_vision_ads/snap_vision_ads/data/uob_image_set/16307461'
+sets = [set1,set2,set3,set4,set5]
+# get all filenames for each set
+
+
+set_paths = []
+
+data = {'path':[],'dom_colours_rgb':[],'dom_colours_lab':[]}
+
+for set in sets:
+    f = []
+    for (dirpath, dirnames, filenames) in walk(set):
+        f.extend(filenames)
+        break
+
+    # set_paths.append(f)
+    for im in f:
+        im_path = set+'/'+im
+        data['path'].append(im_path)
+
+        dominant_colors = get_dominant_colours(im_path, count=5)
+
+        lab_dom_col = []
+        for col in dominant_colors:
+            lab_dom_col.append(color.rgb2lab(col))
+        
+        data['dom_colours_rgb'].append(dominant_colors)
+        data['dom_colours_lab'].append(lab_dom_col)
+
+test_image = {'path':data['path'].pop(0),
+              'dom_colours_rgb':data['dom_colours_rgb'].pop(0),
+              'dom_colours_lab':data['dom_colours_lab'].pop(0)}
+# %%
+for k in range(0,len(data['path'])):
+    comp_im = {'path':data['path'][k],
+              'dom_colours_rgb':data['dom_colours_rgb'][k],
+              'dom_colours_lab':data['dom_colours_lab'][k]}
+    A = np.zeros([5,5])
+
+    i = 0
+    j = 0
+
+    col_data = []
+
+    # for d in data['dom_colours_lab'][0]:
+    for t in range(0,len(test_image['dom_colours_lab'])):
+        row_data = []
+        for c in range(0,len(comp_im['dom_colours_lab'])):
+            row_data.append({'test_col':test_image['dom_colours_lab'][t],
+                    'im_col':comp_im['dom_colours_lab'][c],
+                    'test_col_rgb':test_image['dom_colours_rgb'][t],
+                    'im_col_rgb':comp_im['dom_colours_rgb'][c],
+                    'distance':color.deltaE_cie76(comp_im['dom_colours_lab'][c],
+                    test_image['dom_colours_lab'][t])})
+
+            A[t,c] = color.deltaE_cie76(comp_im['dom_colours_lab'][c],
+                                        test_image['dom_colours_lab'][t])
+        col_data.append(row_data)
+        # create matrix of similarities
+        # test im | image col 1, 2, 3, 4, 5  |
+        #   1     |   1-1, 1-2, 1-3, 1-4, 1-5
+        #   2     |   2-1, 2-2, 2-3, 2-4, 2-5
+        #   3     |   3-1, 3-2, 3-3, 3-4, 3-5
+        #   4     |   4-1, 4-2, 4-3, 4-4, 4-5
+        #   5     |   5-1, 5-2, 5-3, 5-4, 5-5
+    # %%
+    inds_A = scipy.optimize.linear_sum_assignment(A)
+
+    # %%
+
+    # for i in inds_A[0]:
+    #     for j in inds_A[1]:
+    #         col = [col_data[i][j]['test_col_rgb'],col_data[i][j]['im_col_rgb']]
+    #         squares = coloured_square("#%02x%02x%02x" % tuple(int(v * 255) for v in col[0]))
+    #         print(squares,' | Distance: ',col_data[i][j]['distance'])
+    print("Image ",str(k))
+    sum_dist = 0
+    for n in range(0,len(inds_A[0])):
+        i = inds_A[0][n]
+        j = inds_A[1][n]
+        col = [col_data[i][j]['test_col_rgb'],col_data[i][j]['im_col_rgb']]
+        
+        squares = [coloured_square("#%02x%02x%02x" % tuple(int(v * 255) for v in col[0])),coloured_square("#%02x%02x%02x" % tuple(int(v * 255) for v in col[1]))]
+        
+        print(squares[0],squares[1],' | Distance: ',col_data[i][j]['distance'])
+        sum_dist = sum_dist + col_data[i][j]['distance']
+    
+    print("Total Distance: ", sum_dist)
+# cost = np.sum(A[scipy.optimize.linear_sum_assignment(A)])
+# test_pairs = KMeans(n_clusters=n).fit(A).cluster_centers_
+# squares = coloured_square("#%02x%02x%02x" % tuple(int(v * 255) for v in col))
+#         # print(squares)
+
+# # %%
+# if __name__ == "__main__":
+#     import sys
+
+#     try:
+#         path = sys.argv[1]
+#     except ImportError:
+#         sys.exit(f"Usage: {__file__} <PATH>")
+
+# #     # get 5 most dominant colours from an image
+# #     dominant_colors = get_dominant_colours(path, count=5)
+
+# #     lab_dom_col = []
+# #     for col in dominant_colors:
+# #         lab_dom_col.append(color.rgb2lab(col))
+# #         # squares = coloured_square("#%02x%02x%02x" % tuple(int(v * 255) for v in col))
+# #         # print(squares)
+    
+# #     # test against a set of images for now, one correct set, one different
+# #     #  for each image - assign dominant colours to minimise distance overall
+# #     #  need to set some kind of distance threshold which correctly associates good images removes bad
+# #     #  for now - just rank loss from min to max and see what IDs are
+
+# #     color.deltaE_cie76()
+# #     # print(tint_color)
+#     # # print("#%02x%02x%02x" % tuple(int(v * 255) for v in tint_color))
+#     # for n in range(0,len(inds_A[0])):
+#     #     i = inds_A[0][n]
+#     #     j = inds_A[1][n]
+#     #     col = [col_data[i][j]['test_col_rgb'],col_data[i][j]['im_col_rgb']]
+        
+#     #     squares = [coloured_square("#%02x%02x%02x" % tuple(int(v * 255) for v in col[0])),coloured_square("#%02x%02x%02x" % tuple(int(v * 255) for v in col[1]))]
+#     #     print(squares,' | Distance: ',col_data[i][j]['distance'])
+# %%
+# %%
