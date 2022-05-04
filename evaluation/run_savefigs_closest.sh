c59eb182905875822c69584398d2ce5e7659e15d
Matt Clifford <mc15445@my.bristol.ac.uk>
2022-05-04 12:02:27 +0100

    bug

--- evaluation/run_savefigs_closest.sh
+++ evaluation/run_savefigs_closest.sh
@@ -7,5 +7,5 @@ conda activate $VENV
 
 python evaluate_visually.py --save_fig --num_disp 310 --show_case fail --model simple
 python evaluate_visually.py --save_fig --num_disp 310 --show_case pass --model simple
-# python evaluate_visually.py --save_fig --num_disp 310 --show_case fail --model simple_net
-# python evaluate_visually.py --save_fig --num_disp 310 --show_case pass --model simple_net
+python evaluate_visually.py --save_fig --num_disp 310 --show_case fail --model simple_net
+python evaluate_visually.py --save_fig --num_disp 310 --show_case pass --model simple_net
