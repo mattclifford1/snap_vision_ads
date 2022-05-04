f92e8da49173d5f600df50e451e62d0d59cac217
Matt Clifford <mc15445@my.bristol.ac.uk>
2022-05-04 11:59:22 +0100

    eval for new simple model

--- evaluation/run_savefigs_closest.sh
+++ evaluation/run_savefigs_closest.sh
@@ -5,7 +5,7 @@ VENV=snap_vision
 conda activate $VENV
 
 
-python evaluate.py --save_fig --num_disp 310 --show_case fail --model simple
-python evaluate.py --save_fig --num_disp 310 --show_case pass --model simple
-python evaluate.py --save_fig --num_disp 310 --show_case fail --model simple_net
-python evaluate.py --save_fig --num_disp 310 --show_case pass --model simple_net
+python evaluate_visually.py --save_fig --num_disp 310 --show_case fail --model simple
+python evaluate_visually.py --save_fig --num_disp 310 --show_case pass --model simple
+# python evaluate_visually.py --save_fig --num_disp 310 --show_case fail --model simple_net
+# python evaluate_visually.py --save_fig --num_disp 310 --show_case pass --model simple_net
