a9a0d4ab363ccbb25d8b447f49c2c93d1d5284ce
Matt Clifford <mc15445@my.bristol.ac.uk>
2022-05-04 14:28:11 +0100

    eval figs of all nets

--- evaluation/run_savefigs_closest.sh
+++ evaluation/run_savefigs_closest.sh
@@ -5,7 +5,11 @@ VENV=snap_vision
 conda activate $VENV
 
 
-python evaluate_visually.py --save_fig --num_disp 310 --show_case fail --model simple
-python evaluate_visually.py --save_fig --num_disp 310 --show_case pass --model simple
+# python evaluate_visually.py --save_fig --num_disp 310 --show_case fail --model simple
+# python evaluate_visually.py --save_fig --num_disp 310 --show_case pass --model simple
 python evaluate_visually.py --save_fig --num_disp 310 --show_case fail --model simple_net
 python evaluate_visually.py --save_fig --num_disp 310 --show_case pass --model simple_net
+# python evaluate_visually.py --save_fig --num_disp 310 --show_case fail --model facenet
+# python evaluate_visually.py --save_fig --num_disp 310 --show_case pass --model facenet
+# python evaluate_visually.py --save_fig --num_disp 310 --show_case fail --model big_net
+# python evaluate_visually.py --save_fig --num_disp 310 --show_case pass --model big_net
